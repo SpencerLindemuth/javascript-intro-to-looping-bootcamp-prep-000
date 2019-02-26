b47451e7964812937595aa462c23e95add5b1347
Spencer Lindemuth <lindemut@gmail.com>
2019-02-26 02:03:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(foo){
         var bar = [...foo, 'I am 1 strange loop']
       }
       else{
-        var bar = [...foo, ```I am ${i} strange loops```]
+        var bar = [...foo, `I am ${i} strange loops`]
       }
       return bar
     }
