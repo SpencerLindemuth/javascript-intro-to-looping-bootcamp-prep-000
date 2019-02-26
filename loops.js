ffb78ba3ced2a25fbf8a77bc955957161cc75d9c
Spencer Lindemuth <lindemut@gmail.com>
2019-02-26 01:53:21 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(foo){
+    for(var i = 0; i < 25; i++){
+      if(i == 1){
+        var bar = foo.push('I am 1 strange loop.')
+      }
+      else{
+        var bar = foo.push(```I am ${i} strange loops```)
+      }
+      return bar
+    }
+}
